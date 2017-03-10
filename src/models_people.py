dd3972693874b10dd66505627d0acd6fa41be9da
Roberto Antonio Becerra García <rbecerra@centrologic.com>
2017-03-10 13:02:47 +0100

    Fixed bug that prevents login users inside views

--- src/models_people.py
+++ src/models_people.py
@@ -41,7 +41,7 @@ class GenPerson(GenLog, models.Model):  # META: Abstract class
     Defines a person
     '''
     # Control fields
-    user = models.OneToOneField(User, blank=True, null=True)
+    user = models.OneToOneField(User, blank=True, null=True, related_name='person')
     name = models.CharField(_("Name"), max_length=45, blank=False, null=False)
     surname = models.CharField(_("Surname"), max_length=90, blank=False, null=False)
     disabled = models.DateTimeField(_("Disabled from"), null=True, blank=True)
@@ -60,7 +60,7 @@ class GenPerson(GenLog, models.Model):  # META: Abstract class
         else:
             output = "%s*" % (self.user)
         return smart_unicode(output)
-    
+
     def __fields__(self, info):
         fields = []
         fields.append(('user__username', _('User')))
@@ -76,21 +76,21 @@ class GenPerson(GenLog, models.Model):  # META: Abstract class
         l = {}
         # If user is not a superuser, the shown records depends on the profile
         if not info.request.user.is_superuser:
-            
+
             # All the people records shown below must be related with this user
             criterials = []
-            
+
             # The criterials are not exclusives
             if criterials:
                 l["profile_people_limit"] = reduce(operator.or_, criterials)
-        
+
         return l
 
     def __searchF__(self, info):
         tf = {}
         tf['user__is_active'] = (_('Active'), lambda x: Q(user__is_active=x), [(True, _('Yes')), (False, _('No'))])
         return tf
-    
+
     def __searchQ__(self, info, text):
         tf = {}
         tf['username'] = Q(user__username__icontains=text.lower())
@@ -105,23 +105,23 @@ class GenPerson(GenLog, models.Model):  # META: Abstract class
             return bool(self.user.is_superuser or self.user.groups.get(name='Admins'))
         except:
             return False
-    
+
     def profiles(self):
         '''
         return the rolls this people is related with
         '''
         l = []
-        
+
         if self.is_admin():
             l.append(_("Administrator"))
         l.sort()
-        
+
         return l
-    
+
     def delete(self):
         self.clean_memcache()
         return super(GenPerson, self).delete()
-    
+
     def lock_delete(self, request=None):
         if request is not None:
             if request.user == self.user:
@@ -136,7 +136,7 @@ class GenPerson(GenLog, models.Model):  # META: Abstract class
             prefix = hashlib.md5()
             prefix.update(base64.b64encode(settings.SECRET_KEY))
             clean_memcache_item("person:{}".format(self.pk), prefix.hexdigest())
-    
+
     def get_grouppermit(self):
         return self.user.groups.all()
 
@@ -145,7 +145,7 @@ class GenPerson(GenLog, models.Model):  # META: Abstract class
         # Check username
         # Rewrite username
         username = username.lower()
-        
+
         # if exist self.user no modify username
         if not self.user and not username:
             raise ValidationError("This person doesn't have a user assigned, can not change password or email without an user")
@@ -155,13 +155,13 @@ class GenPerson(GenLog, models.Model):  # META: Abstract class
             raise ValidationError("Passwords do not match")
         if password and (len(password) < settings.PASSWORD_MIN_SIZE):
             raise ValidationError("Password can not be smaller than {0} characters".format(settings.PASSWORD_MIN_SIZE))
-        
+
         # Check email
         if email:
             validate_email(email)
         else:
             raise ValidationError("Missing email when saving a new person without a preassigned user")
-        
+
         # Create user and save it to the database
         if self.user:
             # Update password if any
@@ -174,6 +174,6 @@ class GenPerson(GenLog, models.Model):  # META: Abstract class
             self.user = User.objects.create_user(username=username, email=email, password=password)
             # user creator of the user
             self.creator = get_current_user()
-        
+
         # Save changes
         self.user.save()
