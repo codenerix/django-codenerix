239c152c251d9de74c9aa5f15efc913a4a684589
Juanmi Taboada <juanmi@centrologic.com>
2017-04-06 17:54:03 +0200

    CODENERIX setup.py installer, I belive it is still missing some dependency to add folders when installing the package

---
+++ setup.py
@@ -0,0 +1,71 @@
+import os
+from setuptools import setup
+
+#from setuptools.command.install import install
+#class CustomInstallCommand(install):
+#    """Customized setuptools install command - prints a friendly greeting."""
+#    def run(self):
+#        print "Hello, developer, how are you? :)"
+#        install.run(self)
+#        #post-processing code
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-codenerix',
+    version="20170406007",
+    packages=["codenerix"],
+    include_package_data=True,
+    zip_safe=False,
+    license='Apache License Version 2.0',
+    description='Codenerix it is a framework that goes on top of Django so it makes easier development and building of ERPs.',
+    long_description=README,
+    url='https://github.com/centrologic/django-codenerix',
+    author='Juan Miguel Taboada Godoy',
+    author_email='juanmi@centrologic.com',
+    keywords=['django', 'codenerix', 'management','erp','crm'],
+    platforms=['OS Independent'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    # cmdclass={ 'install': CustomInstallCommand, },
+    install_requires = [
+        "pymongo==3.4.0",
+        "django-angular==0.8.4",
+        "bcrypt==3.1.3",
+        "cryptography==1.7.2",
+        "python-dateutil==2.6.0",
+        "paypalrestsdk==1.12.0",
+        "django-recaptcha==1.2.1",
+        "django-rosetta==0.7.6",
+        "scipy==0.14.0",
+        "jsonfield==1.0.3",
+        "openpyxl==2.2.5",
+        "Pillow==2.6.1",
+        "Unidecode==0.4.20",
+        "xhtml2pdf==0.0.6",
+        "Django==1.10.6",
+        "mysqlclient==1.3.6",
+        ],
+    dependency_links = [
+        "git+https://github.com/fle/django-multi-email-field.git#egg=django-multi-email-field",
+        ],
+)
+
