a268631c20863f99da2c1f5fccad91a23b4e39bd
Juanmi Taboada <juanmi@centrologic.com>
2017-04-21 15:50:18 +0200

    New versions

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-codenerix',
-    version="20170406008",
+    version="1.0.0",
     packages=['codenerix'],
     include_package_data=True,
     zip_safe=False,
@@ -59,7 +59,7 @@ setup(
         "xhtml2pdf==0.0.6",
         "Django==1.10.6",
         "django-multi-email-field",
-	"ldap3",
+        "ldap3",
         ],
 )
 
