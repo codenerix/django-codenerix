4ea274c8b63f3e10e2823363c32f37d58580118d
Juanmi Taboada <juanmi@centrologic.com>
2017-04-28 14:55:21 +0200

    Missing developer found :-)

--- codenerix/__init__.py
+++ codenerix/__init__.py
@@ -3,4 +3,5 @@ __version__ = "1.0.11"
 __authors__ = [
     'Juan Miguel Taboada Godoy <juanmi@juanmitaboada.com>',
     'Juan Soler Ruiz <soleronline@gmail.com>',
+    'Francisco Torrejon Puente <frantorrejon@gmail.com>',
 ]
