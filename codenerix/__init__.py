66e1d9225c1edc20919b0cda27f9d747e9104298
Juanmi Taboada <juanmi@centrologic.com>
2017-12-18 00:34:17 +0100

    Setup ready for Django 2.x

--- codenerix/__init__.py
+++ codenerix/__init__.py
@@ -7,7 +7,7 @@ __authors__ = [
 ]
 
 __requirements__ = {
-    'all':[
+    'all': [
         "pymongo",
         "django-angular==0.8.4",
         "python-dateutil",
@@ -17,17 +17,17 @@ __requirements__ = {
         "openpyxl",
         "Pillow",
         "Unidecode",
-        "Django>=1.10.6,<1.11",
+        "Django>=1.10.8",
         "django-multi-email-field",
         "ldap3",
         "django-haystack>=2.6.1",
         "pytz",
         "elasticsearch>=2.0.0,<3.0.0",
     ],
-    '2':[
-        "html5lib==1.0b8", # Default version 0.99999999 is broken with error 'from html5lib import treebuilders, inputstream' => 'ImportError: cannot import name inputstream' (1.0b10 also fails)
+    '2': [
+        "html5lib==1.0b8",  # Default version 0.99999999 is broken with error 'from html5lib import treebuilders, inputstream' => 'ImportError: cannot import name inputstream' (1.0b10 also fails)
     ],
-    '3':[
+    '3': [
         "html5lib",
         ],
     }
