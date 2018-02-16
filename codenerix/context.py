69921722321a13ef6a41cfce968f135edf1a5b86
Juan J Soler <jsoler@centrologic.com>
2018-02-16 10:09:09 +0100

    New function 'context_processors_update' for udpate context

--- codenerix/context.py
+++ codenerix/context.py
@@ -41,65 +41,69 @@ def codenerix(request):
     }
 
 
-def codenerix_js( request ):
+def codenerix_js(request):
     cnf = {}
     
     # Get values
-    CONNECTION_ERROR = getattr(settings,'CONNECTION_ERROR', None)
-    ALARMS_LOOPTIME  = getattr(settings,'ALARMS_LOOPTIME',  None)
-    ALARMS_QUICKLOOP = getattr(settings,'ALARMS_QUICKLOOP', None)
-    ALARMS_ERRORLOOP = getattr(settings,'ALARMS_ERRORLOOP', None)
-    DEBUG = getattr(settings,'DEBUG', False)
+    CONNECTION_ERROR = getattr(settings, 'CONNECTION_ERROR', None)
+    ALARMS_LOOPTIME = getattr(settings, 'ALARMS_LOOPTIME', None)
+    ALARMS_QUICKLOOP = getattr(settings, 'ALARMS_QUICKLOOP', None)
+    ALARMS_ERRORLOOP = getattr(settings, 'ALARMS_ERRORLOOP', None)
+    DEBUG = getattr(settings, 'DEBUG', False)
     DATERANGEPICKER_OPTIONS = getattr(settings, 'DATERANGEPICKER_OPTIONS', None)
     DATETIME_RANGE_FORMAT = getattr(settings, 'DATETIME_RANGE_FORMAT', None)
     
     # Set values
-    if CONNECTION_ERROR is not None: cnf['connection_error'] = CONNECTION_ERROR
-    if ALARMS_LOOPTIME  is not None: cnf['alarms_looptime']  = ALARMS_LOOPTIME
-    if ALARMS_QUICKLOOP is not None: cnf['alarms_quickloop'] = ALARMS_QUICKLOOP
-    if ALARMS_ERRORLOOP is not None: cnf['alarms_errorloop'] = ALARMS_ERRORLOOP
+    if CONNECTION_ERROR is not None:
+        cnf['connection_error'] = CONNECTION_ERROR
+    if ALARMS_LOOPTIME is not None:
+        cnf['alarms_looptime'] = ALARMS_LOOPTIME
+    if ALARMS_QUICKLOOP is not None:
+        cnf['alarms_quickloop'] = ALARMS_QUICKLOOP
+    if ALARMS_ERRORLOOP is not None:
+        cnf['alarms_errorloop'] = ALARMS_ERRORLOOP
     cnf['debug'] = str(DEBUG).lower()
-    cnf['session_cookie_age'] = getattr(settings,'SESSION_COOKIE_AGE', None)
-    cnf['codenerix_css'] = getattr(settings,'CODENERIX_CSS', _('WARNING: CODENERIX_CSS is not set in your configuration!!!'))
-    cnf['codenerix_js']  = getattr(settings,'CODENERIX_JS', _('WARNING: CODENERIX_JS is not set in your configuration!!!'))
+    cnf['session_cookie_age'] = getattr(settings, 'SESSION_COOKIE_AGE', None)
+    cnf['codenerix_css'] = getattr(settings, 'CODENERIX_CSS', _('WARNING: CODENERIX_CSS is not set in your configuration!!!'))
+    cnf['codenerix_js'] = getattr(settings, 'CODENERIX_JS', _('WARNING: CODENERIX_JS is not set in your configuration!!!'))
     
     # Set daterange
     if (DATERANGEPICKER_OPTIONS is None) or (DATETIME_RANGE_FORMAT is None):
         daterangepicker = '"'
         if DATERANGEPICKER_OPTIONS is None:
-            daterangepicker+= "{}".format(_(' WARNING: DATERANGEPICKER_OPTIONS is not set in your configuration!!! '))
+            daterangepicker += "{}".format(_(' WARNING: DATERANGEPICKER_OPTIONS is not set in your configuration!!! '))
         if DATETIME_RANGE_FORMAT is None:
-            daterangepicker+= "{}".format(_(' WARNING: DATETIME_RANGE_FORMAT is not set in your configuration!!! '))
-        daterangepicker+= '"'
+            daterangepicker += "{}".format(_(' WARNING: DATETIME_RANGE_FORMAT is not set in your configuration!!! '))
+        daterangepicker += '"'
         cnf['daterangepicker'] = mark_safe(daterangepicker)
     else:
-        cnf['daterangepicker'] = mark_safe(settings.DATERANGEPICKER_OPTIONS.format(
-                    Format=settings.DATETIME_RANGE_FORMAT[1],
-                    From= _("From"),
-                    To= _("To"),
-                    Apply= _("Apply"),
-                    Cancel= _("Cancel"),
-                    Su= _("Su"),
-                    Mo= _("Mo"),
-                    Tu= _("Tu"),
-                    We= _("We"),
-                    Th= _("Th"),
-                    Fr= _("Fr"),
-                    Sa= _("Sa"),
-                    January= _("January"),
-                    February= _("February"),
-                    March= _('March'),
-                    April= _('April'),
-                    May= _('May'),
-                    June= _('June'),
-                    July= _('July'), 
-                    August= _('August'), 
-                    September= _('September'), 
-                    October= _('October'),
-                    November= _('November'), 
-                    December= _('December')
-                    ))
-    
+        cnf['daterangepicker'] = mark_safe(
+            settings.DATERANGEPICKER_OPTIONS.format(
+                Format=settings.DATETIME_RANGE_FORMAT[1],
+                From=_("From"),
+                To=_("To"),
+                Apply=_("Apply"),
+                Cancel=_("Cancel"),
+                Su=_("Su"),
+                Mo=_("Mo"),
+                Tu=_("Tu"),
+                We=_("We"),
+                Th=_("Th"),
+                Fr=_("Fr"),
+                Sa=_("Sa"),
+                January=_("January"),
+                February=_("February"),
+                March=_('March'),
+                April=_('April'),
+                May=_('May'),
+                June=_('June'),
+                July=_('July'),
+                August=_('August'),
+                September=_('September'),
+                October=_('October'),
+                November=_('November'),
+                December=_('December')
+            )
+        )
     # Return environment
-    return {'cnf': cnf }
-
+    return {'cnf': cnf}
