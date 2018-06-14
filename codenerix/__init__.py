567de5b19b3f196132744e8c63065363b4b10b69
Juanmi Taboada <juanmi@juanmitaboada.com>
2018-06-14 12:08:42 +0200

    New filter to cut strings

--- codenerix/__init__.py
+++ codenerix/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.24"
+__version__ = "1.1.25"
 
 __authors__ = [
     'Juan Miguel Taboada Godoy <juanmi@juanmitaboada.com>',
--- codenerix/static/codenerix/js/filters.js
+++ codenerix/static/codenerix/js/filters.js
@@ -184,4 +184,27 @@ angular.module('codenerixFilters', [])
             return input;
         }
     }
+})
+.filter('cut', function () {
+    return function (value, wordwise, max, tail) {
+        if (!value) return '';
+
+        max = parseInt(max, 10);
+        if (!max) return value;
+        if (value.length <= max) return value;
+
+        value = value.substr(0, max);
+        if (wordwise) {
+            var lastspace = value.lastIndexOf(' ');
+            if (lastspace !== -1) {
+              //Also remove . and , so its gives a cleaner result.
+              if (value.charAt(lastspace-1) === '.' || value.charAt(lastspace-1) === ',') {
+                lastspace = lastspace - 1;
+              }
+              value = value.substr(0, lastspace);
+            }
+        }
+
+        return value + (tail || ' ...');
+    };
 });
